92a37a55bdb0851e11e500ccfd2f64b22ff3270c
hanyu <2544348905@qq.com>
2020-01-31 23:31:59 +0800

    学习中

--- demo-ts/src/index.ts
+++ demo-ts/src/index.ts
@@ -1,16 +1,3 @@
-interface FirstInterface {
-  doSomething(): number
-}
+let someValue: any = "this is a string";
 
-interface SecondInterface {
-  doSomethingElse(): string
-}
-
-class Demo<T extends FirstInterface & SecondInterface> {
-  private genericProperty!: T;
-
-  useT() {
-    this.genericProperty.doSomething()
-    this.genericProperty.doSomethingElse() // 类型“T”上不存在属性“doSomethingElse”
-  }
-}
+let strLength: number = (<string>someValue).length;
