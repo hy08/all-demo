d3c31e475f8426fb28f12527e05f8ca6fba48394
hanyu <2544348905@qq.com>
2020-01-31 23:46:27 +0800

    修改忽略文件

--- demo-ts/src/index.ts
+++ demo-ts/src/index.ts
@@ -1,3 +1,10 @@
 let someValue: any = "this is a string";
 
 let strLength: number = (<string>someValue).length;
+
+function f({ a = "", b = 0 } = {}): void {
+  // ...
+  console.log('a: ' + a + ', b: ' + b)
+}
+
+f();
