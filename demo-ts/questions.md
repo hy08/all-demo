d22db644d901b67610b67892d2f70ebf484760dd
韩雨 <2544348905@qq.com>
2020-03-25 20:08:36 +0800

    ts学习

---
+++ demo-ts/questions.md
@@ -0,0 +1,52 @@
+# TS收纳箱
+
+## 类型兼容性
+ts中的类型是针对结构而非声明，意味着哪怕声明是不同的两个类，只要结构一致也能彼此赋值。
+因此存在兼容性问题。
+
+```dotnetcli
+//问题一：rest参数究竟是什么？rest参数为什么不能赋值给可选参数
+let foo = (x: number, y: number) => { };
+let bar = (x?: number, y?: number) => { };
+let bas = (...args: number[]) => { };
+let bar1 = (x?: number, y?: number, z?: number) => { };
+
+foo = bar;
+foo = bas;
+bar = foo; //error
+bas = foo;
+bar = bas; //error
+bas = bar;
+
+//问题二：为什么这两变量可以相互赋值
+let a: number | undefined;
+let b: undefined = undefined;
+a = b;
+b = a;
+
+//问题三：怎么避免类型声明不同，但是结构相同的兼容问题
+interface ScreenCoordinate {
+  x: number;
+  y: number;
+}
+interface PrintCoordinate {
+  x: number;
+  y: number;
+}
+function sendToPrinter(pt: PrintCoordinate) {
+  // ...
+}
+function getCursorPos(): ScreenCoordinate {
+  return { x: 0, y: 0 };
+}
+
+sendToPrinter(getCursorPos());
+```
+
+## 交叉类型&、联合类型|
+交叉类型用于mixins混入。
+联合类型表示一个值可以是几种类型之一。
+```dotnetcli
+//为什么c类型是never
+var c: number & string;
+```
--- demo-ts/src/index.ts
+++ demo-ts/src/index.ts
@@ -1,7 +1,47 @@
-function fixed(name: string | null): string {
-  function postfix(epithet: string) {
-    return name!.charAt(0) + '.  the ' + epithet; // ok
-  }
-  name = name || "Bob";
-  return postfix("great");
+// let foo = (x: number, y: number) => { };
+// let bar = (x?: number, y?: number) => { };
+// let bas = (...args: number[]) => { };
+// let bar1 = (x?: number, y?: number, z?: number) => { };
+// //问题一：rest参数究竟是什么？rest参数为什么不能赋值给可选参数
+// foo = bar;
+// foo = bas;
+// bar = foo;
+// bas = foo;
+// bar = bas;
+// bas = bar;
+
+
+// let x = (a: number) => 0;
+// let y = (b: number, s: string) => 0;
+
+// y = x; // OK
+
+// //为什么这两变量可以相互赋值
+// let a: number | undefined;
+// let b: undefined = undefined;
+// a = b;
+// b = a;
+
+interface IAnyObject {
+  [prop: string]: any
 }
+
+function mixin<T extends IAnyObject, U extends IAnyObject>(first: T, second: U): T & U {
+  const result = <T & U>{};
+  for (let id in first) {
+    (<T>result)[id] = first[id];
+  }
+  for (let id in second) {
+    if (!result.hasOwnProperty(id)) {
+      (<U>result)[id] = second[id];
+    }
+  }
+
+  return result;
+}
+
+const x = mixin({ a: 'hello' }, { b: 42 });
+
+//为什么c类型是never
+var c: number & string;
+
