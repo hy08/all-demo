6c299f0127b3f8b06c8c7435e0b2aa47b38385b7
韩雨 <2544348905@qq.com>
2020-04-22 18:12:18 +0800

    ts-react 学习

--- my-ts-react/src/layouts/index.tsx
+++ my-ts-react/src/layouts/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.css';
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      <h1 className={styles.title}>React.FC,函数组件泛型接口的别名</h1>
       {props.children}
     </div>
   );
