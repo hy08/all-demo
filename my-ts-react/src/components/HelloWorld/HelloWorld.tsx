3b653c80f001e586150b801ff0c396ce548a1338
韩雨 <2544348905@qq.com>
2020-04-23 17:32:11 +0800

    ts 学习

---
+++ my-ts-react/src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
