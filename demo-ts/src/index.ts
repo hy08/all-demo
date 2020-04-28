dcba189db426ac5817ba9af2849510e2ffc4f2ff
韩雨 <2544348905@qq.com>
2020-04-28 19:55:08 +0800

    ts 学习

--- demo-ts/src/index.ts
+++ demo-ts/src/index.ts
@@ -10,6 +10,7 @@
 // bar = bas;
 // bas = bar;
 
+
 // let x = (a: number) => 0;
 // let y = (b: number, s: string) => 0;
 
@@ -58,6 +59,11 @@ enum Color {
 }
 let red: number = Color.Red;
 
+interface Person {
+  name: string;
+  age: number;
+  location?: string;
+}
 
 type mixin = { name: string } & { age: number };
 
@@ -109,3 +115,100 @@ type AddTodoAction = ReturnType<typeof addTodo>
 
 // type K3 = keyof { [x: string]: string, name: boolean };
 // const p: K3 = 1;
+
+class Images {
+  public src: string = 'https://www.google.com.hk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+  public alt: string = '谷歌'
+  public width: number = 500
+}
+
+type propsNames = keyof Images
+
+type propsType = Images[propsNames]
+
+function pick<T, K extends keyof T>(o: T, names: K[]): T[K][] {
+  return names.map(n => o[n]);
+}
+const user = {
+  username: 'Jessica Lee',
+  id: 460000201904141743,
+  token: '460000201904141743',
+  avatar: 'http://dummyimage.com/200x200',
+  role: 'vip'
+}
+const res = pick(user, ['token', 'id'])
+
+interface User {
+  username: string
+  id: number
+  token: string
+  avatar: string
+  role: string
+}
+type partial<T> = { [K in keyof T]?: T[K] }
+type partialUser = partial<User>
+
+declare function f<T extends boolean>(x: T): T extends true ? string : number;
+
+const x = f(Math.random() < 0.5)
+const y = f(false)
+const z = f(true)
+
+type WrappedUsage<T> = [T] extends [boolean] ? "YES" : "NO";
+type NotDistributed = WrappedUsage<number | boolean>
+
+type Filter<T, U> = T extends U ? T : never;
+type R1 = Filter<string | number | (() => void), Function>;
+
+interface Part {
+
+  id: number;
+  name: string;
+  subparts: Part[];
+  updatePart(newName: string): void;
+}
+type FunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? K : never }[keyof T]
+type R = FunctionPropertyNames<Part>;
+
+interface People {
+  id: string
+  name: string
+  age?: number
+  from?: string
+}
+type NullableKeys<T> = { [K in keyof T]: undefined extends T[K] ? K : never }[keyof T];
+type RR = Exclude<NullableKeys<People>, undefined>;
+
+
+class BeeKeeper {
+  hasMask: boolean;
+}
+
+class ZooKeeper {
+  nametag: string;
+}
+
+class Animal {
+  numLegs: number;
+}
+
+class Bee extends Animal {
+  keeper: BeeKeeper;
+}
+
+class Lion extends Animal {
+  keeper: ZooKeeper;
+}
+
+function createInstance<A extends Animal>(c: new () => A): A {
+  return new c();
+}
+
+createInstance(Lion).keeper.nametag;  // typechecks!
+createInstance(Bee).keeper.hasMask;   // typechecks!
+
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
+
+type Result = UnionToIntersection<{ a: string } | { b: number }>;
+
