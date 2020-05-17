f1498fdd2bb5f801ff5b627cec6e1cee84a9c98f
hanyu <2544348905@qq.com>
2020-05-17 19:31:42 +0800

    引入公共style.js

--- vue-pages/src/pages/00-system-demo/00-module-demo/index.ts
+++ vue-pages/src/pages/00-system-demo/00-module-demo/index.ts
@@ -1,11 +1,8 @@
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from '@/modules';
-Vue.use(ElementUI);
-
+import '../../../common/style';
 Vue.config.productionTip = false;
 
 new Vue({
