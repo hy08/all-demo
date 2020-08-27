310193ee97b4b92d1073ee4114cc804da3ece4cd
hanyu <2544348905@qq.com>
2020-08-27 08:11:51 +0800

    vue ts

--- vue-demo/src/pages/00-system-demo/00-module-demo/mixins/hello.ts
+++ vue-demo/src/pages/00-system-demo/00-module-demo/mixins/hello.ts
@@ -5,6 +5,10 @@ import Component from 'vue-class-component';
 // You can declare mixins as the same style as components.
 @Component
 export class Hello extends Vue {
+  /**
+   *  mixin中的响应式数据
+   */
   mixinText = 'Hello mixins';
+
   obj: { name: string } = { name: 'han' };
 }
--- vue-demo/src/pages/00-system-demo/00-module-demo/views/index.vue
+++ vue-demo/src/pages/00-system-demo/00-module-demo/views/index.vue
@@ -3,19 +3,26 @@
 </template>
 
 <script lang="ts">
-  import { Vue, Component, Mixins } from 'vue-property-decorator';
-  import { Log } from '../decorator.js';
-  import { Hello } from '../mixins/hello';
-
-  @Component
-  export default class Index extends Mixins(Hello) {
-    created() {
-      this.hello('hello');
-      console.log(this.mixinText, this.obj.name);
-    }
-    @Log
-    hello(value) {
-      console.log(value);
-    }
+import { Vue, Component, Mixins } from 'vue-property-decorator';
+import { Log } from '../decorator.js';
+import { Hello } from '../mixins/hello';
+type Info = {
+  name: string;
+  age: number;
+};
+@Component
+export default class Index extends Mixins(Hello) {
+  //data定义，类属性
+  message = 'hello world';
+  info: Info = { name: 'hy', age: 25 };
+  //如果数据的值是undefined,则不会成为响应式数据
+  created() {
+    this.hello('hello');
+    console.log(this.mixinText, this.obj.name);
   }
+  @Log
+  hello(value) {
+    console.log(value);
+  }
+}
 </script>
